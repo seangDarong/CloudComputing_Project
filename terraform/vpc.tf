c78faeef53b8e08e252b790451c64edef93fa2dd
praseth002 <prasethsokha@gmail.com>
2026-04-04 13:41:42 +0700

    fix: vpc.tf typos pt.2

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "private_1" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.3.0/24"
     availability_zone = "ap-southeast-1a"
-    tags = {Name = "private-subnet-2"}
+    tags = {Name = "private-subnet-1"}
 }
 resource "aws_subnet" "private_2" {
     vpc_id = aws_vpc.main.id
