3ea6eb0b83009332c229d04eea086c10448c2a56
praseth002 <prasethsokha@gmail.com>
2026-04-04 13:40:09 +0700

    fix: vpc.tf typos

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,28 +10,27 @@ resource "aws_subnet" "public_1" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "ap-southeast-1a"
-    map_customer_owned_ip_on_launch = true
+    map_public_ip_on_launch = true
     tags = {Name = "public-subnet-1"}
 }
 resource "aws_subnet" "public_2" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.2.0/24"
     availability_zone = "ap-southeast-1b"
-    map_customer_owned_ip_on_launch = true
+    map_public_ip_on_launch = true
     tags = {Name = "public-subnet-2"}
 }
+
 resource "aws_subnet" "private_1" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.3.0/24"
     availability_zone = "ap-southeast-1a"
-    map_customer_owned_ip_on_launch = true
     tags = {Name = "private-subnet-2"}
 }
 resource "aws_subnet" "private_2" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.4.0/24"
     availability_zone = "ap-southeast-1b"
-    map_customer_owned_ip_on_launch = true
     tags = {Name = "private-subnet-2"}
 }
 
