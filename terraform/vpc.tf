cbee84871dc8fbb50cdad9357a5b65129ca7cace
praseth002 <prasethsokha@gmail.com>
2026-04-04 12:58:34 +0700

    feat: added vpc

---
+++ terraform/vpc.tf
@@ -0,0 +1,61 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = { Name = "project-vpc" }
+}
+
+resource "aws_subnet" "public_1" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "ap-southeast-1a"
+    map_customer_owned_ip_on_launch = true
+    tags = {Name = "public-subnet-1"}
+}
+resource "aws_subnet" "public_2" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "ap-southeast-1b"
+    map_customer_owned_ip_on_launch = true
+    tags = {Name = "public-subnet-2"}
+}
+resource "aws_subnet" "private_1" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "ap-southeast-1a"
+    map_customer_owned_ip_on_launch = true
+    tags = {Name = "private-subnet-2"}
+}
+resource "aws_subnet" "private_2" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.4.0/24"
+    availability_zone = "ap-southeast-1b"
+    map_customer_owned_ip_on_launch = true
+    tags = {Name = "private-subnet-2"}
+}
+
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id
+    tags = {Name = "project-igw"}
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+
+    tags = {Name = "public-rt"}
+}
+
+resource "aws_route_table_association" "public_1" {
+    subnet_id = aws_subnet.public_1.id
+    route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public_2" {
+    subnet_id = aws_subnet.public_2.id
+    route_table_id = aws_route_table.public.id
+}
