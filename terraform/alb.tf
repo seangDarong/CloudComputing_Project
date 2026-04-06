17702822a5ca6282126befadcebec6dd37dc506e
praseth002 <prasethsokha@gmail.com>
2026-04-06 12:53:24 +0700

    fix: formatiing

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,34 +1,34 @@
 resource "aws_lb" "main" {
-    name = "${var.project_name}-alb"
-    internal = false
-    load_balancer_type = "application"
-    security_groups = [aws_security_group.alb.id]
-    subnets = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+  name               = "${var.project_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
 
-    tags = {Name = "${var.project_name}-alb"}
+  tags = { Name = "${var.project_name}-alb" }
 }
 
 resource "aws_lb_target_group" "web" {
-    name = "${var.project_name}-tg"
-    port = 3000
-    protocol = "HTTP"
-    vpc_id = aws_vpc.main.id
+  name     = "${var.project_name}-tg"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
 
-    health_check {
-      path = "/"
-      unhealthy_threshold = 2
-      healthy_threshold = 2
-      interval = 30
-    }
+  health_check {
+    path                = "/"
+    unhealthy_threshold = 2
+    healthy_threshold   = 2
+    interval            = 30
+  }
 }
 
 resource "aws_lb_listener" "http" {
-    load_balancer_arn = aws_lb.main.arn
-    port = 80
-    protocol = "HTTP"
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
 
-    default_action {
-      type = "forward"
-      target_group_arn = aws_lb_target_group.web.arn
-    }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,54 +7,54 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public_1" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "ap-southeast-1a"
-    map_public_ip_on_launch = true
-    tags = {Name = "public-subnet-1"}
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-southeast-1a"
+  map_public_ip_on_launch = true
+  tags                    = { Name = "public-subnet-1" }
 }
 resource "aws_subnet" "public_2" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.2.0/24"
-    availability_zone = "ap-southeast-1b"
-    map_public_ip_on_launch = true
-    tags = {Name = "public-subnet-2"}
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-southeast-1b"
+  map_public_ip_on_launch = true
+  tags                    = { Name = "public-subnet-2" }
 }
 
 resource "aws_subnet" "private_1" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.3.0/24"
-    availability_zone = "ap-southeast-1a"
-    tags = {Name = "private-subnet-1"}
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-southeast-1a"
+  tags              = { Name = "private-subnet-1" }
 }
 resource "aws_subnet" "private_2" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.4.0/24"
-    availability_zone = "ap-southeast-1b"
-    tags = {Name = "private-subnet-2"}
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "ap-southeast-1b"
+  tags              = { Name = "private-subnet-2" }
 }
 
 resource "aws_internet_gateway" "main" {
-    vpc_id = aws_vpc.main.id
-    tags = {Name = "${var.project_name}-igw"}
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "${var.project_name}-igw" }
 }
 
 resource "aws_route_table" "public" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.main.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
 
-    tags = {Name = "public-rt"}
+  tags = { Name = "public-rt" }
 }
 
 resource "aws_route_table_association" "public_1" {
-    subnet_id = aws_subnet.public_1.id
-    route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
 }
 resource "aws_route_table_association" "public_2" {
-    subnet_id = aws_subnet.public_2.id
-    route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
 }
